3ab4bfa823cb582b89c5179749fd6b5c9604a467
Mykyta Mazurenko <surprisedsunrise@gmail.com>
2023-10-12 13:57:42 +0300

    image banner restyle

--- src/components/Homepage/ImageBanner.tsx
+++ src/components/Homepage/ImageBanner.tsx
@@ -1,28 +1,66 @@
-import { Carousel } from 'flowbite-react';
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 
 export default function ImageBanner({ className }: { className?: string }) {
-  const images = ['/assets/catPurple.png', '/assets/textPurple.png'];
+  const image1Ref = useRef(null);
+  const image2Ref = useRef(null);
+  useEffect(() => {
+    const tl = gsap.timeline({ repeat: -1, yoyo: true, repeatDelay: 10 });
+    tl.to(
+      image1Ref.current,
+      {
+        x: '100%',
+        opacity: 0,
+        duration: 2
+      },
+      '+=1'
+    ).fromTo(
+      image2Ref.current,
+      {
+        x: '-100%',
+        opacity: 0,
+        duration: 2
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 2
+      },
+      '-=1'
+    );
+
+    return () => {
+      tl.kill();
+    };
+  }, []);
 
   return (
-    <div
-      className={`top-0 w-full ${className} inset-0 flex items-center justify-center md:absolute md:left-44 md:top-[-30%]`}
-    >
-      <Carousel
-        className='pointer-events-none'
-        slideInterval={10000}
-        indicators={false}
-        leftControl=' '
-        rightControl=' '
+    <>
+      <div
+        className={`top-0 w-full ${className} inset-0 flex items-center justify-center md:absolute md:left-44 md:top-[-30%]`}
       >
-        {images.map((image, index) => (
-          <img
-            key={index}
-            className={`z-0 h-full w-full object-contain transition-opacity delay-150 md:w-auto `}
-            alt='banner-image'
-            src={image}
-          />
-        ))}
-      </Carousel>
-    </div>
+        <Image
+          ref={image2Ref}
+          className={`z-0 h-full w-full  object-contain opacity-0 md:w-auto`}
+          alt='banner-image'
+          src='/assets/textPurple.png'
+          width={1000}
+          height={1000}
+        />
+      </div>
+      <div
+        className={`top-0 w-full ${className} absolute inset-0 flex items-center justify-center md:left-44 md:top-[-30%]`}
+      >
+        <Image
+          ref={image1Ref}
+          className={`z-0 h-full w-full object-contain md:w-auto`}
+          alt='banner-image'
+          src='/assets/catPurple.png'
+          width={1000}
+          height={1000}
+        />
+      </div>
+    </>
   );
 }
