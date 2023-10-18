98881a3d84439567b582e9ba9cf2690ae97e7aff
Vlad-Makarenko <makrenkovladok@gmail.com>
2023-10-18 22:15:30 +0300

    add scroll to from order now button

--- src/components/buttons/OrderNowButton.tsx
+++ src/components/buttons/OrderNowButton.tsx
@@ -1,15 +1,32 @@
-import { ButtonHTMLAttributes } from 'react';
+import gsap from 'gsap';
+import { ButtonHTMLAttributes, useEffect, useState } from 'react';
 import './styles/OrderNowButton.css';
 interface OrderNowButtonProps extends ButtonHTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
 export default function OrderNowButton(props: OrderNowButtonProps) {
-  const { className, onClick } = props;
+  const { className } = props;
+
+  const [sectionId, setSectionId] = useState('');
+  useEffect(() => {
+    if (sectionId.length)
+      gsap.to(window, {
+        duration: 1,
+        scrollTo: {
+          y: sectionId,
+          autoKill: false
+        }
+      });
+    setSectionId('');
+  }, [sectionId]);
+
   return (
     <div
       className={`${className} box translate-x-0 cursor-pointer rounded-xl bg-gray-600/50 px-5 py-4 text-center font-coiny text-2xl text-secondary shadow [@media(min-width:1000px)]:text-[25px] [@media(min-width:1200px)]:text-4xl`}
-      onClick={onClick}
+      onClick={() => {
+        setSectionId('#contactUs');
+      }}
     >
       Order now!
     </div>
