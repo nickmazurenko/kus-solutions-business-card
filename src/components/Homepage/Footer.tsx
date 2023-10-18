cab587125dfd5963384f33a13b59f425b6e88a6a
Vlad-Makarenko <makrenkovladok@gmail.com>
2023-10-18 22:05:27 +0300

    add scroll to from footer

--- src/components/Homepage/Footer.tsx
+++ src/components/Homepage/Footer.tsx
@@ -1,5 +1,7 @@
+import gsap from 'gsap';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { PiInstagramLogo, PiLinkedinLogo, PiTelegramLogo, PiTiktokLogo } from 'react-icons/pi';
 
 interface HomepageFooterProps {
@@ -8,6 +10,20 @@ interface HomepageFooterProps {
 
 export default function HomepageFooter(props: HomepageFooterProps) {
   const { className } = props;
+  const [sectionId, setSectionId] = useState('');
+
+  useEffect(() => {
+    if (sectionId.length)
+      gsap.to(window, {
+        duration: 1,
+        scrollTo: {
+          y: '#banner' === sectionId ? 0 : sectionId,
+          autoKill: false
+        }
+      });
+    setSectionId('');
+  }, [sectionId]);
+
   return (
     <div
       className={`${className} flex h-full w-full flex-col items-center justify-center gap-10 p-10`}
@@ -17,34 +33,83 @@ export default function HomepageFooter(props: HomepageFooterProps) {
         contacts
       </div>
       <div className='flex flex-row gap-10 text-5xl text-secondary md:text-6xl'>
-        <Link href='https://www.instagram.com/' className='cursor-pointer'>
+        <Link
+          href='https://www.instagram.com/'
+          className='cursor-pointer text-secondary hover:text-primary'
+          target='_blank'
+        >
           <PiInstagramLogo />
         </Link>
-        <Link href='https://linkedin.com/' className='cursor-pointer'>
+        <Link
+          href='https://linkedin.com/'
+          className='cursor-pointer text-secondary hover:text-primary'
+          target='_blank'
+        >
           <PiLinkedinLogo />
         </Link>
-        <Link href='https://telegram.com/' className='cursor-pointer'>
+        <Link
+          href='https://telegram.com/'
+          className='cursor-pointer text-secondary hover:text-primary'
+          target='_blank'
+        >
           <PiTelegramLogo />
         </Link>
-        <Link href='https://tiktok.com/' className='cursor-pointer'>
+        <Link
+          href='https://tiktok.com/'
+          className='cursor-pointer text-secondary hover:text-primary'
+          target='_blank'
+        >
           <PiTiktokLogo />
         </Link>
       </div>
-      <span className='self-center font-dongle text-5xl text-primary md:self-start md:px-20'>
+      <span className='cursor-default self-center font-dongle text-5xl text-primary md:self-start md:px-20'>
         Service
       </span>
       <div className='flex w-full flex-row-reverse items-center justify-between font-dongle text-3xl text-secondary md:flex-row md:px-20'>
         <div className='flex flex-col text-end md:text-start'>
-          <div>Technologies</div>
-          <div>Examples</div>
-          <div>Reviews</div>
-          <div>About Us</div>
+          <div
+            onClick={() => {
+              setSectionId('#technologies');
+            }}
+            className={`cursor-pointer  text-secondary hover:text-primary`}
+          >
+            Technologies
+          </div>
+          <div
+            onClick={() => {
+              setSectionId('#examples');
+            }}
+            className={`cursor-pointer  text-secondary hover:text-primary`}
+          >
+            Examples
+          </div>
+          <div
+            onClick={() => {
+              setSectionId('#reviews');
+            }}
+            className={`cursor-pointer  text-secondary hover:text-primary`}
+          >
+            Reviews
+          </div>
+          <div
+            onClick={() => {
+              setSectionId('#aboutUs');
+            }}
+            className={`cursor-pointer  text-secondary hover:text-primary`}
+          >
+            About Us
+          </div>
         </div>
         <div className='flex flex-col'>
           <span className='underline'>Email</span>
           <span>Number</span>
-          <div className='opacity-0'>placeholder</div>
-          <div className='flex flex-row'>
+          <div className='cursor-default opacity-0'>placeholder</div>
+          <div
+            className='flex cursor-pointer flex-row '
+            onClick={() => {
+              setSectionId('#banner');
+            }}
+          >
             <Image
               width={50}
               height={50}
